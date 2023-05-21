bc0e21ef917b453633d453b047fc160a073c05fd
niniel <natalia.maria.rojas@gmail.com>
2023-05-20 22:11:36 -0600

    pruebas

--- lab_1_moviles/lib/main.dart
+++ lab_1_moviles/lib/main.dart
@@ -1,126 +1,148 @@
 import 'package:flutter/material.dart';
-import 'package:lab_1_moviles/view/YourNameWidget.dart';
+import 'package:lab_1_moviles/graph_ql.dart';
+import 'package:lab_1_moviles/model.dart';
+import 'package:lab_1_moviles/util.dart';
+import 'package:graphql/client.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(GraphQLApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class GraphQLApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:  YourNameWidget() /*const MyHomePage(title: 'Flutter Demo Home Page')*/,
+      title: 'GraphQL',
+      home: MainScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MainScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MainScreenState createState() => _MainScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MainScreenState extends State<MainScreen> {
+  final _controller = TextEditingController();
+  final _loading = ValueNotifier(false);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final _characters = <Character>[];
+
+  late GraphQLClient _client;
+
+  void handleResult(QueryResult result) {
+    print(result.data);
+    if (result.data != null) {
+      _characters.add(Character.toObject(result.data?['character'], Utils.randomColor()));
+
+    }
+    _loading.value = false;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _client = GraphQL('https://graphql.anilist.co').getClient();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _loading.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    final size = MediaQuery.of(context).size;
+    return Material(
+      child: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(
+              left: size.width * 0.1,
+              right: size.width * 0.1,
+              top: 30,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            child: Card(
+              elevation: 10,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                child: TextFormField(
+                  onFieldSubmitted: (query) async {
+                    _loading.value = true;
+                    _controller.clear();
+                    handleResult(await _client.queryCharacter(query));
+                  },
+                  controller: _controller,
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Search Anime Characters'),
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+          Expanded(
+            child: ValueListenableBuilder(
+              valueListenable: _loading,
+              builder: (context, value, child) {
+                return Stack(
+                  children: [
+                    GridView.builder(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 30,
+                        horizontal: 30,
+                      ),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                        childAspectRatio: 0.7,
+                      ),
+                      itemBuilder: (context, index) {
+                        final character = _characters[index];
+                        return Card(
+                          elevation: 15,
+                          child: Stack(
+                            fit: StackFit.expand,
+                            children: [
+                              Image.network(character.image, fit: BoxFit.cover),
+                              Column(
+                                children: [
+                                  Expanded(child: SizedBox()),
+                                  Container(
+                                    width: size.width,
+                                    color: character.color,
+                                    child: Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 20),
+                                      child: Center(
+                                        child: Text(
+                                          character.fullName,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w600,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                      itemCount: _characters.length,
+                    ),
+                    if (value) Center(child: CircularProgressIndicator())
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
